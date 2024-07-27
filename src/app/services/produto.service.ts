ff820927440b146a10a0b8b7df8022f8f46b928e
Leandro Amaral <lramaral02m@gmail.com>
2024-07-27 00:37:39 -0300

    resolvido problema de response do produto disponivel

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,9 +1,9 @@
+import { Produto } from './../models/Produtos';
 import { Injectable } from '@angular/core';
 import { enviroment } from '../enviroments/enviroment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Response } from '../models/Response';
-import { Produto } from '../models/Produtos';
 
 @Injectable({
   providedIn: 'root'
